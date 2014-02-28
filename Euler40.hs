7292d95dc04ae8a2029ffa1e3bb149be4da326a2
Uli Köhler <ulikoehler@online.de>
2014-02-28 01:10:02 +0100

    40

--- Euler40.hs
+++ Euler40.hs
@@ -4,5 +4,9 @@ champernowne =
     let cnext n = (show n) ++ cnext (n+1)
     in cnext 1
 
+-- Champernowne digit n
+cnum :: Int -> Int
+cnum n = read [champernowne !! (n - 1)]
+
 main = do
-    print $ take 50 champernowne
+    print $ product $ map cnum [1, 10, 100, 1000, 10000, 100000, 1000000]
