779dea359719b4a7cb9b1b051e5dcf5b8aa1b9ae
Uli Köhler <ulikoehler@online.de>
2014-05-03 00:31:49 +0200

    Euler50 works now

--- Euler50.hs
+++ Euler50.hs
@@ -1,16 +1,44 @@
+{-# LANGUAGE NoMonomorphismRestriction, BangPatterns #-}
 import Math.NumberTheory.Primes.Sieve
 import Math.NumberTheory.Primes.Testing
 import Data.List
-import Data.Ord
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
 import Control.Parallel.Strategies
 
-yourFunction = concatMap inits . tails
-    where inits = takeWhile (not . null) . iterate init
-          tails = takeWhile (not . null) . iterate tail
+limit = 1000000
 
+{-# NOINLINE primeSet #-}
+primeSet :: IntSet
+primeSet = IntSet.fromList $ map fromIntegral $ takeWhile (< limit) primes
+
+myIsPrime :: Int -> Bool
+myIsPrime i = IntSet.member i primeSet
+
+consecutivesFromI :: Int -- ^ Starting index (0-based)
+                  -> [Int] -- ^ Original list
+                  -> [[Int]] -- ^ List of lists
+consecutivesFromI i xs =
+    let list' = drop i xs
+        len = length list'
+    in map (`take` list') [2..len]
+
+sumLen x = (sum x, length x)
+
+-- Filters
+primeAndLimit x = fstLimit x && fstIsPrime x
+fstIsPrime = myIsPrime . fromIntegral . fst
+fstLimit = (>) limit . fst
+
+consecSumLen xs n =
+    let sumLens = map sumLen $ consecutivesFromI n xs
+        limitedSumLens = takeWhile ((>) limit . fst) sumLens
+    in filter fstIsPrime limitedSumLens
 
 main = do
-    let lim = 1000000
-    let basePrimes = takeWhile (<=lim) primes
-    let primeList = maximumBy (comparing length) $ filter ((<=lim) . sum) $ filter (isPrime . sum) $ yourFunction basePrimes
-    print $ (length primeList, sum primeList)
+    let !relevantPrimes = IntSet.toList $ primeSet
+    let !numPrimes = length relevantPrimes
+    -- n primes, starting at prime i (i: 0-based)
+    let parMapped = concat $ parMap rdeepseq (consecSumLen relevantPrimes) [0..numPrimes]
+    let longestSumPrime = fst $ maximumBy (\(_,l1) (_,l2) -> compare l1 l2) parMapped
+    print longestSumPrime
