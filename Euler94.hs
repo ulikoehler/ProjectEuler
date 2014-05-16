20ba27cbe2c2bc6e1e14d9b5061e01251df4d412
Uli Köhler <ulikoehler@online.de>
2014-05-16 22:52:15 +0200

    Fix some 94 issues

--- Euler94.hs
+++ Euler94.hs
@@ -1,26 +1,26 @@
 
 
-data Triangle = Triangle Int Int Int deriving (Show)
+data Triangle = Triangle Integer Integer Integer deriving (Show)
 
-perimeter :: Triangle -> Int
+perimeter :: Triangle -> Integer
 perimeter (Triangle a b c) = a + b + c
 
 isSquare n = sq * sq == n
     where sq = floor $ sqrt $ (fromIntegral n :: Double)
 
 isAlmostEquilateral t@(Triangle a b c) =
-    let p = perim / 2
+    let p = perim `div` 2
         perim = perimeter t
-    in isSquare (p * (p - a) * (p - b) * (p - c))
+    in even perim && isSquare (p * (p - a) * (p - b) * (p - c))
 
-limit = 1000000000 :: Int
+limit = 1000000000 :: Integer
 
 genTriangles a = [Triangle a a (a-1), Triangle a a (a+1)]
 
 allTriangles =
     concatMap genTriangles [1..]
 
-relevantTriangles = takeWhile ((>) limit . perimeter) allTriangles
+relevantTriangles = takeWhile ((>=) limit . perimeter) allTriangles
 
 main = do
     let aeTriangles = filter isAlmostEquilateral $ relevantTriangles
