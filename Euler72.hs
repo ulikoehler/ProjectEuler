355858d7feacb04a11132efc0e0ac5fdebc06cb5
Uli Köhler <ulikoehler@online.de>
2015-12-27 03:18:02 +0100

    72

--- Euler72.hs
+++ Euler72.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+import Math.Sieve.Phi
 import Prelude hiding ((//))
 import Fraction
 import qualified Data.Set as S
 
-space = [1..1000]
-fractions = S.fromList [a // b | a <- space, b <- space, a < b]
+stop = 1000000
+space = [2..stop]
+numReducedFractions =
+    let sv = sieve stop
+    in sum [phi sv a | a <- space]
 
 main = do
-    print $ S.size fractions
+    print $ numReducedFractions
