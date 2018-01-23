903674a16b423c9214f1759070f952b719f26c71
Uli Köhler <ulikoehler@online.de>
2018-01-23 04:43:51 +0100

    Try to fix 549 memory issue

--- Euler549.py
+++ Euler549.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from sympy import factorint
+import gc
 
 def factorial_coeffs(n):
     return list(range(0, n+1))
@@ -8,7 +9,10 @@ def factorial_factorization_up_to(n):
     ctr = Counter()
     for coeff in range(1, n+1):
         yield Counter(ctr)
+        print(coeff)
         ctr += Counter(factorint(coeff))
+        if coeff % 10000 == 0:
+            gc.collect()
     yield ctr
 
 def factorizable(fact1, fact2):
@@ -19,7 +23,7 @@ def factorizable(fact1, fact2):
 
 #factortable = [factorial_factorization(n) for n in range(1000)]
 
-factortable = list(factorial_factorization_up_to(10000))
+factortable = list(factorial_factorization_up_to(45000))
 
 def s(n):
     fact1 = factorint(n)
@@ -31,4 +35,4 @@ def s(n):
 def S(n):
     return sum(s(i) for i in range(2,n+1))
 
-print(S(10000))
+print(S(100000))
