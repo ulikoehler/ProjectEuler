ae060c13e275d543acbccb0f6150f1c3ccdd2e3b
Uli Köhler <ulikoehler@online.de>
2018-01-28 00:32:55 +0100

    Euler207 works now but is too slow

--- Euler207.py
+++ Euler207.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
+import numpy as np
+import fractions
 
+def t(k):
+    # wolfram alpha: solve 4^t=2^t+k for t:
+    return np.log(0.5*(np.sqrt(4*k+1) + 1))/np.log(2)
 
+def isnat(n):
+    return np.abs(n - np.rint(n)) < 1e-6
 
-def x(m):
-    for k in range(1, m)
-        t = 0
-        while True:
-            4**n - 2**n
+ns = np.arange(1,1000000+1)
+ts = t(ns)
+isvalid = isnat(4**ts)
+isint = isnat(ts)
+
+def P(m):
+    return fractions.Fraction(np.sum(isint[:m]), np.sum(isvalid[:m]))
+
+#print(P(5))
+#print(P(10))
+#print(P(15))
+#print(P(20))
+#print(P(25))
+#print(P(30))
+#print(P(180))
+#print(P(185))
+
+for m in range(2, ns.shape[0]):
+    if P(m) < fractions.Fraction(1,1234567):
+        print("Result",m)
+    if m % 100000 == 0:
+        print(m)
